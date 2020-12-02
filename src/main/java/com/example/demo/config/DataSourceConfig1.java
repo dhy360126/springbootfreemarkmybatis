86f9a812fcb1111df99651b9091acc4ad7639636
duanhy <duanhy@asiainfo.com>
2020-12-02 10:24:28 +0800

    sqlSessionFactoryRef 改为sqlsessiontemplate就行了，本来老是启动报错，改为这个就可以了，感觉还是有点奇怪

--- src/main/java/com/example/demo/config/DataSourceConfig1.java
+++ src/main/java/com/example/demo/config/DataSourceConfig1.java
@@ -13,14 +13,14 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
-@Configuration
+@Configuration 
 //@MapperScan(basePackages = "com.example.demo.dao.db1",sqlSessionFactoryRef = "db1SqlsessionFactory")
 @MapperScan(basePackages = "com.example.demo.dao.db1",sqlSessionTemplateRef = "db1SqlSessionTemplate")
 public class DataSourceConfig1 {
 /*1.使用@Bean可以创建一个bean对象交给spring容器管理sql
 2.@Bean创建的bean对象的名称默认为方法名，也可以指定
 3.@Bean方法参数表示，接收一个bean对象，默认按照type类型接收注入的对象，若要修改为byName方式，可以使用@Qualifier注解注入准确的对象
-4.@Primary表示该bean为此类型的默认bean，在其他地方引用的时候用@Autowired即可按照类型注入，不受同类型多个对象影响*/
+4.@Primary 表示该bean为此类型的默认bean，在其他地方引用的时候用@Autowired即可按照类型注入，不受同类型多个对象影响*/
     @Bean("db1DataSource")
     @ConfigurationProperties(prefix = "spring.datasource.test")
     public DataSource db1DataSource(){
